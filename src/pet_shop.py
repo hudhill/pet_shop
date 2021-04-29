cdf7c0d083ffeaa8efc2a1b11b93ab7420038e0e
hudhill <sam.hudhill@gmail.com>
2021-04-29 18:10:55 +0100

    first round done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,48 @@
-# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, value):
+    pet_shop["admin"]["total_cash"] = get_total_cash(pet_shop) + value
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, value):
+    pet_shop["admin"]["pets_sold"] = get_pets_sold(pet_shop) + value
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets.append(pet)
+    return pets
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def remove_pet_by_name(pet_shop, name):
+    if find_pet_by_name(pet_shop, name)["name"] == name:
+        pet_shop["pets"].remove(find_pet_by_name(pet_shop, name))
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, remove_value):
+    customer["cash"] = get_customer_cash(customer) - remove_value
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
